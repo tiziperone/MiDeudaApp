a6d4df7e011f204906d2cba5ac4726a77f5c73e5
tiziperone <tiziperonetobias@gmail.com>
2026-08-17 12:15:43 -0300

    Se configuró la inyeccion de dependencias

--- MiDeuda.App/MauiProgram.cs
+++ MiDeuda.App/MauiProgram.cs
@@ -1,25 +1,35 @@
 ﻿using Microsoft.Extensions.Logging;
+using MiDeuda.Core; // Importa tu librería en VB
+using System.IO;
 
-namespace MiDeuda.App
+namespace MiDeuda.App;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        // 1. Definir la ruta multiplataforma de la base de datos
+        string dbPath = Path.Combine(FileSystem.AppDataDirectory, "mideuda.db");
+
+        // 2. Registrar el servicio de base de datos de MiDeuda.Core
+        builder.Services.AddSingleton<DatabaseService>(s => ActivatorUtilities.CreateInstance<DatabaseService>(s, dbPath));
+
+        // 3. Registrar las vistas que consumirán el servicio
+        builder.Services.AddTransient<MainPage>();
+
+        return builder.Build();
     }
 }
