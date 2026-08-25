6e26eb0d9ab9241a4a21ef52063477c3568d583a
tiziperone <tiziperonetobias@gmail.com>
2026-08-25 09:56:33 -0300

    Se agregó una clase para convertir si es dolar

---
+++ MiDeuda.App/EsDolarConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace MiDeuda.App;
+
+public class EsDolarConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value?.ToString()?.ToUpperInvariant() == "USD";
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
