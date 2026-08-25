64ad75e7fa8c3671deceee52c61bef33a3e95556
tiziperone <tiziperonetobias@gmail.com>
2026-08-25 09:40:22 -0300

    Se creó el archvio "CotizacionDolar.vb" para poder convertir a pesos la moneda que se seleccione (usd)

---
+++ MiDeuda.Core/CotizacionDolar.vb
@@ -0,0 +1,9 @@
+﻿Public Class CotizacionDolar
+    Public Property moneda As String
+    Public Property casa As String
+    Public Property nombre As String
+    Public Property compra As Decimal
+    Public Property venta As Decimal
+    Public Property fechaActualizacion As DateTime
+
+End Class
