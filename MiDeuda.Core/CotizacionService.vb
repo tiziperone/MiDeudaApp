25723783de35cd3527205a34917dd405272f1e2c
tiziperone <tiziperonetobias@gmail.com>
2026-08-25 09:41:42 -0300

    Se agregó una clase con una api de dolar

---
+++ MiDeuda.Core/CotizacionService.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Net.Http
+Imports System.Text.Json
+Imports System.Threading.Tasks
+
+Public Class CotizacionService
+    Private Shared ReadOnly client As New HttpClient()
+
+    Public Shared Async Function ObtenerPrecioVentaDolarAsync(Optional tipo As String = "oficial") As Task(Of Decimal)
+        Dim url As String = $"https://dolarapi.com/v1/dolares/{tipo.ToLower()}"
+        Try
+            Dim response As HttpResponseMessage = Await client.GetAsync(url)
+            response.EnsureSuccessStatusCode()
+
+            Dim jsonString As String = Await response.Content.ReadAsStringAsync()
+            Dim cotizacion As CotizacionDolar = JsonSerializer.Deserialize(Of CotizacionDolar)(jsonString)
+
+            Return cotizacion.venta
+        Catch ex As Exception
+            Throw New Exception("No se pudo obtener la cotización actual: " & ex.Message)
+        End Try
+    End Function
+End Class
